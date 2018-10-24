9134418204e7a22264fd1e76f3894bafd9321d85
alex <a@oneseventyfour.com>
2018-10-24 15:02:16 -0700

    still write ascii to serial, add test mode

--- src/axis_controller/utils/serial_interface_util.rb
+++ src/axis_controller/utils/serial_interface_util.rb
@@ -23,15 +23,19 @@ class SerialInterfaceUtil
 
 
   def initialize(port:, baud: 38400, data_bits: 8, parity: :even)
-    @serconn = Serial.new(port, baud, data_bits, parity)
+
+    @serconn = if ENV["TEST"]
+      TestSerConn.new()
+    else
+      Serial.new(port, baud, data_bits, parity)
+    end
 
     run
   end
 
   def run
-    while 1
-      input = get_input
-      k=7
+    while get_input
+      #party!!
     end
   end
 
@@ -61,23 +65,26 @@ class SerialInterfaceUtil
   def proc_help()
     puts "Dumbass.... "
     puts CMD_DESC.map{|k,v| "#{k}: #{v}"}.join("\n")
+    return true
   end
 
   def proc_read(address)
-    msg = SerialDriveMsg.new(:cmd => 0x03, :address => address.to_i, :data_2 => 0x01)
+    msg = SerialDriveMsg.new(:cmd => 0x03, :address => address.to_i, :data => 0x01)
     serconn.write(msg.to_msg)
     puts wait_for_response
+
+    return true
   end
 
   def proc_write(address, value, do_wait_for_response = false)
-    msg = SerialDriveMsg.new(:cmd => 0x06, :address => address.to_i, data_2: value.to_i)
+    msg = SerialDriveMsg.new(:cmd => 0x06, :address => address.to_i, data: value.to_i)
     serconn.write(msg.to_msg)
 
     if do_wait_for_response
       puts wait_for_response
-    else
-      return
     end
+
+    return true
 
   end
 
@@ -101,8 +108,9 @@ class SerialInterfaceUtil
   end
 
   def proc_exit(exit="yeah")
+    puts "BYE!"
     exit
-    return nil
+    return false
   end
 end
 
@@ -111,17 +119,18 @@ class SerialDriveMsg
     @address = 0x01
     @cmd = options[:cmd] || 0x03
     data_address = options.fetch(:address)
-    @data_address_hi, @data_address_lo = ("%04X" % data_address).to_s.split('').each_slice(2).to_a.map{|i| i.join.to_i(16)}
-    @data_1 = options.fetch(:data_1, 0x00)
-    @data_2 = options.fetch(:data_2, 0x00)
+    @data_address_hi, @data_address_lo = split_4b_hex(data_address)
+
+    @data_1, @data_2 = split_4b_hex(options.fetch(:data, 0x0001))
+
   end
 
   def to_hx(item)
-    sprintf("%02X", item).split('').map{|i| "%02X" % i.ord}.join("")
+    sprintf("%02X", item)#.split('').map{|i| "%02X" % i.ord}.join("")
   end
 
   def to_msg()
-    str = "%02X" % ':'.ord
+    str = ':'
     str << to_hx(@address)
     str << to_hx(@cmd)
     str << to_hx(@data_address_hi)
@@ -129,7 +138,7 @@ class SerialDriveMsg
     str << to_hx(@data_1)
     str << to_hx(@data_2)
     str << to_hx(checksum)
-    str << "0D0A"
+    str << "\r\n"
     str
   end
 
@@ -137,7 +146,30 @@ class SerialDriveMsg
     (-(@address + @cmd + @data_address_lo + @data_1 + @data_2) & 0xFF)
   end
 
+  private
+
+  #For a given hex represented in 4 bytes (0x0000), split them into halves and return the associated integers.
+  def split_4b_hex(num)
+    return ("%04X" % num).to_s.split('').each_slice(2).to_a.map{|i| i.join.to_i(16)}
+  end
+
 end
+
+class TestSerConn
+
+  def read(bytes)
+    return ":)"
+  end
+
+  def write(msg)
+    puts "TEST SER WRITES"
+    puts msg
+  end
+
+end
+
+#WRITE 4040 to 178 - :010600B20FC870
+#      2020          :010600B207E45C
 
 
 SerialInterfaceUtil.start(ARGV)
