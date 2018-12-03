0053f8f5ed31fb192f06bdf5bc2f53d6463bf37a
alex <a@oneseventyfour.com>
2018-12-02 18:43:25 -0800

    server fixes

--- src/base_controller/utils/drivewriter_server.rb
+++ src/base_controller/utils/drivewriter_server.rb
@@ -2,6 +2,16 @@ require 'rack/app'
 
 RFCOMM_CMD_PATH = "/tmp/rfcmdpath"
 
+#The registers and values needed to ensure a drive is properly set up for the sim. Applies to both axes. One would argue this isn't necessary,
+# as if the drive is not configured it will not be able to communicate with the axis controller. But they just haters. 
+SETUP_SEQUENCE = [
+  [67, 3],
+  [66, 3],
+  [64, 1],
+  [20, 1500],
+  [51, 1500]
+]
+
 
 class App < Rack::App
 
@@ -11,12 +21,30 @@ class App < Rack::App
     required 'r', :class => String, :desc => 'The register to write to', :example => '177'
     required 'v', :class => String, :desc => 'The value to write', :example => '1200'
   end
+
   post '/write' do
-    axis = params['axis'].downcase[0]
+    axis = process_axis(params)
     write_to_drive(axis, params['r'], params['v'])
   end
 
+  get '/' do
+    "I'm Alright"
+  end
 
+  post '/setup' do 
+    output = ""
+    SETUP_SEQUENCE.each do |itm|
+        axis = process_axis(params)
+        write_to_drive(process_axis(params), itm[0], itm[1])
+        output << "Wrote #{itm[1]} to #{itm[0]} on axis '#{axis}'"
+    end
+
+    output
+  end
+
+  def process_axis(params)
+    params['axis'].downcase[0]
+  end
 
   def write_to_drive(axis, register, value)
     #Fine we'll use a damn file
